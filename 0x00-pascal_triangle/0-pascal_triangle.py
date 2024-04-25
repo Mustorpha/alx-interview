b0e7b1db768895af815268c781b96426e8e19a3c
~thebigmuse~ <mustorphajamiu@gmail.com>
2024-04-25 10:07:26 +0100

    Fix commenting issue for checker

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""
-0-pascal_triangle
-"""
+"""0-pascal_triangle"""
 
 
 def pascal_triangle(n):
-    """Generate a list of lists of intergers representing the pascal's triangle
-
-    Args:
-        n (int): the number of rows in the triangle
-
-    Return:
-        triangle (list): a list of list representing Pascal triangle
-    """
+    """Generate a list of lists of intergers representing the pascal's triangle"""
+    triangle = []
+    
     if n <= 0:
-        return []
+        return triangle
     else:
-        triangle = []
         for row in range(n):
             if row == 0:
                 triangle.append([1])
@@ -25,6 +17,7 @@ def pascal_triangle(n):
             else:
                 next_row = [1]
                 prev_row = triangle[-1]
+                
                 for i in range(len(prev_row)-1):
                     next_row.append(prev_row[i] + prev_row[i+1])
                 next_row.append(1)
