ea92aa9d1b72384136f7d43ac0f7663dd4f50a63
~thebigmuse~ <mustorphajamiu@gmail.com>
2024-04-26 10:42:39 -0700

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,9 +3,9 @@
 
 
 def pascal_triangle(n):
-    """Generate a list of lists of intergers representing the pascal's triangle"""
+    """Get a list of lists of intergers representing the pascal's triangle"""
     triangle = []
-    
+
     if n <= 0:
         return triangle
     else:
@@ -13,14 +13,13 @@ def pascal_triangle(n):
             if row == 0:
                 triangle.append([1])
             elif row == 1:
-                triangle.append([1,1])
+                triangle.append([1, 1])
             else:
                 next_row = [1]
                 prev_row = triangle[-1]
-                
+
                 for i in range(len(prev_row)-1):
                     next_row.append(prev_row[i] + prev_row[i+1])
                 next_row.append(1)
                 triangle.append(next_row)
         return triangle
-
