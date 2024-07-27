c4ec8930b70cbe0d8e27570c3173cc900e9c2364
Mustorpha Okikiola Jamiu <mjamiu@innovastra.com>
2024-07-27 13:49:47 +0100

    n queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,65 +1,44 @@
 #!/usr/bin/python3
-"""0-nqueens"""
-
+""" N queens """
 import sys
 
-def is_safe(board, row, col, n):
-    """Checks if a queen can be placed at a given position on the board without being attacked by any other queen.
 
-    Args:
-        board (list): A list of column indices where queens have been placed so far.
-        row (int): The current row being processed.
-        col (int): The current column being considered.
-        n (int): The size of the board.
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    Returns:
-        bool: True if the queen can be placed at the given position, else False.
-    """
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
-            return False
-    return True
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-def solve_nqueens(n, row=0, board=[]):
-    """Solves the N queens problem using a recursive backtracking approach.
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-    Args:
-        n (int): The size of the board.
-        row (int, optional): The current row being processed. Defaults to 0.
-        board (list, optional): A list of column indices where queens have been placed so far. Defaults to an empty list.
+n = int(sys.argv[1])
 
-    Returns:
-        None
-    """
-    if row == n:
-        print([[i, col] for i, col in enumerate(board)])
-        return
-    for col in range(n):
-        if is_safe(board, row, col, n):
-            board.append(col)
-            solve_nqueens(n, row + 1, board)
-            board.pop()
 
-def main():
-    """The main function that checks the command-line arguments and calls the solve_nqueens function.
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
-    Returns:
-        None
-    """
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    solve_nqueens(n)
 
-if __name__ == "__main__":
-    main()
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
